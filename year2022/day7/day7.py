fa84ea013400adbeb6bf2723ed4f30e19a1abb75
DorukCem <114157471+DorukCem@users.noreply.github.com>
2023-06-06 13:48:31 +0300

    Add files via upload

--- year2022/day7/day7.py
+++ year2022/day7/day7.py
@@ -2,40 +2,50 @@ def take_input(file_name) -> list[str]:
    with open(file_name, "r") as file:
       return [i.rstrip('\n') for i in file]
 
+# Directory names are not unique
+# I can say for example xyz/zzz/xyz
+# Here xyz and xyz do not refer to the same directory
 
 def parse_lines(lines : list[str]):
    directories : dict[str : int] = {}
    current_path = []
+   files = set()
 
    for line in lines:
 
-      match line.strip().split(" ", maxsplit=1):
-         case ['$', command]:
-            match command.split(" "):
-               case ['cd', '/']:
-                  current_path.clear()
-               case ['cd', '..']:
-                  current_path.pop()
-               case ['cd', dir_name]:
-                  current_path.append(dir_name)
-               case ['ls']:
-                  pass
-               
+      match line.strip().split(" "):
+         case ['$', 'cd', '/']:
+            current_path.clear()
          
+         case ['$', 'cd', '..']:
+            current_path.pop()
+         
+         case ['$', 'cd', dir_name]:
+            current_path.append(dir_name)
+         
+         case ['$', 'ls']:
+            continue
+
          case ['dir', directory]:
-            if directory not in directories:
-               directories[directory] = 0
+            full_director_name = "/".join(current_path) + "/" + directory if current_path else directory
+
+            if full_director_name not in directories:
+               directories[full_director_name] = 0      
          
          case [size, file]:
-            for dir in current_path:
-               directories[dir] += int(size)
-
+            if file not in files:
+               files.add(file)
+               for i, _ in enumerate(current_path):
+                  dir_path = current_path[:i+1]
+                  dir_name = "/".join(dir_path)
+                  directories[dir_name] += int(size)
+      
    return directories
 
 def sum_sizes(directories : dict[str: int]) -> int:
    sum = 0
    for value in directories.values():
-      if value < 100000 :
+      if value <= 100000 :
          sum+= value
    return sum
 
