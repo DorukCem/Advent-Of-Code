ab235f8952057b1a89a95907476a85d6f25c4f32
DorukCem <114157471+DorukCem@users.noreply.github.com>
2023-06-08 01:05:26 +0300

    Could not learn how to git in vscode

--- year2022/day7/day7.py
+++ year2022/day7/day7.py
@@ -5,16 +5,17 @@ def take_input(file_name) -> list[str]:
 # Directory names are not unique
 # I can say for example xyz/zzz/xyz
 # Here xyz and xyz do not refer to the same directory
+# Same can be said for file names
 
 def parse_lines(lines : list[str]):
-   directories : dict[str : int] = {}
-   current_path = []
+   directories : dict[str : int] = {"home":0}
+   current_path = ["home"]
    files = set()
 
    for line in lines:
       match line.strip().split(" "):
          case ['$', 'cd', '/']:
-            current_path.clear()
+            current_path = ["home"]
          
          case ['$', 'cd', '..']:
             current_path.pop()
@@ -49,6 +50,7 @@ def sum_sizes(directories : dict[str: int]) -> int:
          sum+= value
    return sum
 
+
 def part1():
    print(
       sum_sizes(
@@ -58,4 +60,27 @@ def part1():
          )
       )
 
+#part2
+ 
+def find_smallest_dir_above_n(directories : dict[str: int]) -> int:
+   TOTAL_SIZE = 70000000
+   NEEDED_SIZE = 30000000
+   space_left = TOTAL_SIZE - directories["home"]
+   space_needed = NEEDED_SIZE - space_left
+   min_dir = float("inf")
+   for value in directories.values():
+      if value >= space_needed:
+         min_dir = min(min_dir, value)
+   return min_dir
+
+def part2():
+   print(
+      find_smallest_dir_above_n(
+         parse_lines(
+            take_input("input.txt")
+            )
+         )
+      )
+
 part1()
+part2()
