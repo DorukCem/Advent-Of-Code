c8b6252977f0b27b6d855a56028375cba713b8c6
Doruk Cem <doruk.cem@gmail.com>
2023-12-02 10:17:12 +0300

    part2

--- Rust-2023/src/bin/day1.rs
+++ Rust-2023/src/bin/day1.rs
@@ -9,9 +9,9 @@ fn read_lines(filename: &str) -> Vec<String> {
    result
 }
 
-fn main() {
+fn part1() {
    let mut sum = 0;
-   let lines = read_lines("inputs/in_01.txt");
+   let lines: Vec<String> = read_lines("D:/Advent-Of-Code/Rust-2023/inputs/in_01.txt");
    for line in lines {
       let mut digits : Vec<u32> = Vec::new();
       for c in line.chars() {
@@ -23,5 +23,35 @@ fn main() {
    }
 
    println!("{sum}");
+} 
+
+fn part2() {
+   let lines: Vec<String> = read_lines("D:/Advent-Of-Code/Rust-2023/inputs/in_01.txt");
+   let mut sum = 0;
+   for line in lines {
+      let replaced = line
+         .replace("zero", "zero0zero")
+         .replace("one", "one1one")
+         .replace("two", "two2two")
+         .replace("three", "three3three")
+         .replace("four", "four4four")
+         .replace("five", "five5five")
+         .replace("six", "six6six")
+         .replace("seven", "seven7seven")
+         .replace("eight", "eight8eight")
+         .replace("nine", "nine9nine");
+      
+      let digits  = replaced.chars().filter_map(|c| c.to_digit(10)).collect::<Vec<u32>>();
+      
+      sum += digits.first().unwrap() * 10 + digits.last().unwrap();
+
+   }
+
+   println!("{sum}")
+}
+
+fn main() {
+   part1();
+   part2();
 }
 
