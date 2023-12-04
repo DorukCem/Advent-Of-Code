1efc23a0fdc49eea6d495963f74479586f1e7ebb
Doruk Cem <doruk.cem@gmail.com>
2023-12-04 13:30:44 +0300

    part2

--- Rust-2023/src/bin/day4.rs
+++ Rust-2023/src/bin/day4.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn read_lines(filename: &str) -> Vec<String> {
    use std::fs::read_to_string;
 
@@ -9,13 +11,10 @@ fn read_lines(filename: &str) -> Vec<String> {
    result
 }
 
-fn main(){
-   let lines: Vec<String> = read_lines("D:/Advent-Of-Code/Rust-2023/inputs/ex_04.txt");
+fn part1(lines: &Vec<String> ) {
    let mut sum = 0;
    for line in lines {
-      
       let nums = line.split(":").nth(1).unwrap();
-
       let punch_card: Vec<Vec<_>> = nums
          .split("|")
          .map(|c| c.split_whitespace().collect())
@@ -33,4 +32,33 @@ fn main(){
    println!("part1: {sum}");
 }
 
-// ! Possible edge case: dulciate numbers
+fn part2(lines: Vec<String> ) {
+   let mut num_cards : HashMap<usize, i32> = HashMap::new();
+   let mut sum = 0;
+   for (i, line) in lines.iter().enumerate() {
+      let nums = line.split(":").nth(1).unwrap();
+      let punch_card: Vec<Vec<_>> = nums
+         .split("|")
+         .map(|c| c.split_whitespace().collect())
+         .collect();
+      
+      let winning_nums = punch_card.first().unwrap();
+      let my_nums = punch_card.last().unwrap();
+      let count = winning_nums.iter().filter(|c| my_nums.contains(c)).count();
+      
+      let iter =  num_cards.get(&i).unwrap_or(&0);
+      for _ in 0..iter+1 {
+         sum += 1;
+         for x in i+1..i+count+1 {
+            *num_cards.entry(x).or_insert(0) += 1;
+         }
+      }
+   }
+   println!("part2: {:#?}", sum);
+}
+
+fn main(){
+   let lines: Vec<String> = read_lines("D:/Advent-Of-Code/Rust-2023/inputs/in_04.txt");
+   part1(&lines);
+   part2(lines);
+}
