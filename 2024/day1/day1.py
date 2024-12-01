c3aaed7807cedfdc052ba952dd05dca566545215
Doruk <doruk.cem@gmail.com>
2024-12-01 11:03:29 +0300

    both parts

---
+++ 2024/day1/day1.py
@@ -0,0 +1,19 @@
+with open("day1/input.txt", "r") as f:
+    left_col = []
+    right_col = []
+    for line in f.readlines():
+        l, r = [int(x) for x in line.split("  ")]
+        left_col.append(l)
+        right_col.append(r)
+
+    left_col.sort()
+    right_col.sort()
+
+    part1 = sum(
+        [abs(x - y) for x, y in zip(left_col, right_col)],
+    )
+
+    part2 = sum([num * right_col.count(num) for num in set(left_col)])
+
+    print(part1)
+    print(part2)
