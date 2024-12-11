df9947448bef877fa32504baba19c6eae9ef6853
Doruk <doruk.cem@gmail.com>
2024-12-11 14:25:43 +0300

    part2

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -1,3 +1,4 @@
+from collections import Counter, defaultdict
 import math
 
 stone_cache = {}
@@ -20,26 +21,35 @@ def get_next(stone: int):
         return (int(as_str[: len(as_str) // 2]), int(as_str[len(as_str) // 2 :]))
     return 2024 * stone
 
-def flatten(l):
-    new = []
-    for x in l:
-        match x:
-            case (a, b):
-                new.append(a)
-                new.append(b)
-            case c:
-                new.append(x)
-    return new
 
-with open("2024/day11/example.txt", "r") as f:
+with open("2024/day11/input.txt", "r") as f:
     stones = [int(x) for x in f.read().split()]
+    stones = dict(Counter(stones))
+    stones = defaultdict(int, stones)
     part1 = None
+
+    # We use dict to hold the rocks as keys
+    # The value for the key indicates the number of time that rock is present 
+    # If we have a key K1 which has value V, applying the function to K1
+    # produces K2. Since we know that K1 exists V number of times, 
+    # K2 will also be produced V number of times
+    # So we can say for the next iteration 
+    # dict[K2] += V 
+    # (if key K2 is not present default dict will assign 0 to it and then add our result)
+
     for i in range(75):
+        new = defaultdict(int)
         if i == 25:
-            part1 = len(stones)
-        new = [get_next_cached(x) for x in stones]
-        stones = flatten(new)
-        print(i)
-    part2 = len(stones)
+            part1 = sum(stones.values())
+        for k, v in stones.items():
+            match get_next_cached(k):
+                case (a, b):
+                    new[a] += v
+                    new[b] += v
+                case one_rock:
+                    new[one_rock] += v
+        stones = new
+
+    part2 = sum(stones.values())
     print(part1)
     print(part2)
